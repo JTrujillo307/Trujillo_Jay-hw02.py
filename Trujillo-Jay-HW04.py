3ed11a44241b33ab939bd58f4f089c6e135ebfe4
JTrujillo307 <jtruji18@uwyo.edu>
2024-11-12 18:06:28 -0700

    Update Trujillo-Jay-HW04.py

--- Trujillo-Jay-HW04.py
+++ Trujillo-Jay-HW04.py
@@ -9,5 +9,13 @@ lines = []
 use = []
 with open("promt.txt") as file, open("out2.txt","w") as newfiles:
   for lines in file:
-    line = lines.replace("/t" , " ").split(" ")
-    tem = " "
+    line = lines.replace("\t" , " ").split(" ")
+    tem = ""
+    for key in line:
+      use = key.split(":")
+      if(len(use)== "w"):
+        if(use[0]== "w"):
+          tem += (" "*int(use[1]))
+        else:
+          tem +=("*"*int(use[1]))
+    newfile.write(tem + "\n")
